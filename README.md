0384f1d3c5e7ef15b27444c36cf25e4cf87660c9
GedsonMonteiro <decleidson2@gmail.com>
2021-04-15 15:10:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<p align="center">
+<img src="vue-icon.png" alt="drawing" width="100"/>
+</p>
+
+---
+
+## LISTA DE TAREFAS - TODO LIST
+### LITE v1.0
+
+<br>
+Lista de tarefas em html e css, com a aplicação do Vue.Js.<br><br>
+
+### EXECUÇÃO
+- Instalação do Vue: https://vuejs.org/v2/guide/installation.html<br>
+
+Com NPM
+```
+npm install -g @vue/cli
+```
+
+Com YARN
+```
+yarn global add @vue/cli<br>
+```
+
+- Execute o prompt de comando na pasta onde será criado o projeto e em seguida, crie o projeto executando o comando: 
+```
+vue create "nomedoprojeto" (SEM AS ASPAS)
+```
+
+Para esse projeto, foi usado a versão 3 do Vue.
+
+- O próprio comando nos informa como podemos rodar o projeto. Execute o comando no terminal conforme visto ao final da criação do projeto:
+```
+cd "nome da pasta do projeto"
+npm run serve
+```
+
+- Para finalizar a execução do projeto, use CTRL+C.
+---
---
+++ todolist-lite-v1/src/components/TodoList.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <div class="app">
+    <ol>
+      <li v-for="todo in todos" v-bind:key="todo.id"> <!-- O v-bind:key="todo.id" é para cada li tem um id unico. -->
+        <p>{{ todo.text }}</p> <!-- O {{ todo.text }} vai fazer o <p></p> renderizar o texto da tarefa. -->
+      </li>
+    </ol>
+
+    <div class="inputs">
+      <input placeholder="Digite a tarefa" v-model="text" /> <!-- O v-model vai nos auxiliar a pegar o dado do input de adicionar tarefa, guardando em uma variavel. -->
+      <div class="buttons">
+        <button type="submit" v-on:click="addTodo(text)">Nova Tarefa</button> <!-- O v-on:click vai executar uma função caso o botão seja clicado. -->
+        <button type="submit" v-on:click="removeTodos()">Excluir Lista</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [{
+        id: 1, 
+        text: '',
+        todos: [],
+      }],
+    };
+  },
+  methods: {
+    addTodo(text) {
+      if (text !== " ") {
+        this.todos.push({
+          id: this.todos.lenght+1,
+          text,
+        });
+        this.text = '';
+      }
+    },
+    removeTodos(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<!-- Adicione o atributo "scoped" para limitar o CSS apenas a este componente -->
+<style scoped>
+
+</style>
